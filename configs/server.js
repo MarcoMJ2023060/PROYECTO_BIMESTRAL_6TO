5c91a593035ecb0908853ef065db2593f55b0bba
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2025-02-12 22:26:17 -0600

    funcionalidades basicas de Usuarios

--- configs/server.js
+++ configs/server.js
@@ -5,12 +5,21 @@ import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
 import { dbConexion } from "./mongo.js"
+import authRoutes from "../src/auth/auth.routes.js"
+import userRoutes from "../src/user/user.routes.js"
+import apiLimiter from "../src/middlewares/rate-limit-validator.js"
 
 const middlewares = (app) =>{
     app.use(express.json())
     app.use(cors())
     app.use(helmet())
     app.use(morgan("dev"))
+    app.use(apiLimiter)
+}
+
+const routes = (app) =>{
+    app.use("/proyectoBimestral/v1/auth", authRoutes)
+    app.use("/proyectoBimestral/v1/user", userRoutes)
 }
 
 const conectarDB = async () =>{
@@ -27,6 +36,7 @@ export const inicioServidor = () =>{
     try{
         middlewares(app)
         conectarDB()
+        routes(app)
         app.listen(process.env.PORT)
         console.log(`SERVER CORRIENDO EN EL PUERTO ${process.env.PORT}`)
     }catch(err){
