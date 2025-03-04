6aa9ad790af0b93bb97da4dd449606182f317386
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2025-03-03 18:41:33 -0600

    implementacion de Admin por defecto y categoria por defecto

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import { config } from "dotenv"
 import { inicioServidor } from "./configs/server.js"
+import { categoryDefault } from "./src/category/category.controller.js"
+import {adminDefault} from "./configs/adminDefault.js"
+
 
 config()
 inicioServidor()
+categoryDefault()
+adminDefault()
