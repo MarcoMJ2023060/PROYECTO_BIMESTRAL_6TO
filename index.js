cedc7b9a272619e94df98be50f2a1dcdfc805c35
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2025-03-03 19:19:17 -0600

    se corrigieron errores en index y category controller, se añade dependencia bcrypt para el admin default ademas de una foto para un cliente que ingrese

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { config } from "dotenv"
 import { inicioServidor } from "./configs/server.js"
 import { categoryDefault } from "./src/category/category.controller.js"
-import {adminDefault} from "./configs/adminDefault.js"
+import { crearAdmin} from "./configs/adminDefault.js"
 
 
 config()
 inicioServidor()
 categoryDefault()
-adminDefault()
+crearAdmin()
