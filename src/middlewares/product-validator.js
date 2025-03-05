5e7f88e2928599bdfa4f3490a2441e04db9f7049
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2025-03-04 21:35:34 -0600

    se agrega middleware para validar token adicional al middleware que valida los roles, agrego validar por token y por rol ADMIN_ROLE a algunos endpoints de productos

--- src/middlewares/product-validator.js
+++ src/middlewares/product-validator.js
@@ -2,8 +2,12 @@ import { body, param } from "express-validator";
 import { validarCampos } from "./validar-campos.js";
 import { productExists } from "../helpers/db-validators.js";
 import { handleErrors } from "./handle-errors.js";
+import {validateJWT} from "./validate-jwt.js" ;
+import {hasRoles} from "./validate-roles.js";
 
 export const registrarProductosValidador = [
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
     body("name").not().isEmpty().withMessage("NOMBRE ES REQUERIDO"),
     body("description").not().isEmpty().withMessage("DESCRIPCION ES REQUERIDA"),
     body("price").not().isEmpty().withMessage("PRECIO ES REQUERIDO").isNumeric().withMessage("EL PRECIO DEBE SER NUMERO"),
@@ -13,6 +17,8 @@ export const registrarProductosValidador = [
 ];
 
 export const editarProductoValidador = [
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
     body("name").optional().not().isEmpty().withMessage("NOMBRE ES REQUERIDO"),
     body("description").optional().not().isEmpty().withMessage("DESCRIPCION ES REQUERIDA"),
     body("price").optional().not().isEmpty().withMessage("PRECIO ES REQUERIDO").isNumeric().withMessage("EL PRECIO DEBE SER NUMERO"),
@@ -22,21 +28,29 @@ export const editarProductoValidador = [
 ];
 
 export const incrementarInventarioValidador = [
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
     body("cantidad").notEmpty().withMessage("CANTIDAD ES REQUERIDA").isNumeric().withMessage("LA CANTIDAD DEBE SER NUMERO"),
     validarCampos
 ]
 
 export const decrementarInventarioValidador = [
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
     body("cantidad").notEmpty().withMessage("CANTIDAD ES REQUERIDA").isNumeric().withMessage("LA CANTIDAD DEBE SER NUMERO"),
     validarCampos
 ]
 
 export const productosAgotadosValidador = [
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
     body("stock").notEmpty().withMessage("STOCK INVALIDO").isNumeric().withMessage("EL STOCK DEBE SER UN NUMERO"),
     validarCampos
 ]
 
 export const eliminarProductoValidador = [
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
     param("uid").isMongoId().withMessage("NO ES UN ID VALIDO"),
     param("uid").custom(productExists),
     validarCampos,
