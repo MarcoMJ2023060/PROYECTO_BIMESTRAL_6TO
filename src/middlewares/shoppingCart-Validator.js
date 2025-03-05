d86536b64a424923400b945ddea966d4160462b1
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2025-03-04 23:52:44 -0600

    SE AGREGARON VALIDACIONES DE ROLES PARA FUNCIONES DE CARRITO DE COMPRAS ADICIONAL SE AÑADE AUTORIZACION POR TOKEN

--- src/middlewares/shoppingCart-Validator.js
+++ src/middlewares/shoppingCart-Validator.js
@@ -2,8 +2,12 @@ import {body, param} from "express-validator"
 import { validarCampos } from "./validar-campos.js"
 import { handleErrors } from "./handle-errors.js"
 import { codeExist, shoppingCartExist } from "../helpers/db-validators.js"
+import { validateJWT } from "./validate-jwt.js"
+import { hasRoles } from "./validate-roles.js"
 
 export const registrarCarritoCompraValidador = [
+    validateJWT,
+    hasRoles("CLIENT_ROLE"),
     body("user").not().isEmpty().withMessage("User is required").isMongoId().withMessage("This id is'nt valid"),
     body("products").notEmpty().withMessage("The products is required").custom(codeExist),
     validarCampos,
@@ -11,12 +15,16 @@ export const registrarCarritoCompraValidador = [
 ];
 
 export const pagarCarritoValidador =[
+    validateJWT,
+    hasRoles("CLIENT_ROLE"),
     param("uid").notEmpty().withMessage("The uid is required").custom(shoppingCartExist),
     validarCampos,
     handleErrors
 ]
 
 export const editarCarritoValidador = [
+    validateJWT,
+    hasRoles("CLIENT_ROLE"),
     param("uid").notEmpty().withMessage("The uid is required").custom(shoppingCartExist),
     body("addProduct").optional().custom(codeExist),
     body("deleteProduct").optional().custom(codeExist),
@@ -25,6 +33,8 @@ export const editarCarritoValidador = [
 ]
 
 export const eliminarCarritoValidador = [
+    validateJWT,
+    hasRoles("CLIENT_ROLE"),
     param("uid").notEmpty().withMessage("The uid is required").custom(shoppingCartExist),
     validarCampos,
     handleErrors
